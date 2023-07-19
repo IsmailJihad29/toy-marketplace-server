ef5ce8b9bdf4d89a8504eefa7cfc878600f7fd38
ismailJihad29 <ismailhossenjihad234@gmail.com>
2023-07-19 07:14:26 +0600

    server is runnig

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const port = process.env.PORT || 5000
+
+// middleware 
+app.use(express.json())
+app.use(cors())
+
+
+
+app.get('/', (req, res) => { 
+    res.send('Figlandia Server Is running ')
+})
+
+
+
+app.listen(port, () => {
+    console.log(`Figladia Server is listening on ${port}`)
+})
