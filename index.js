bd05f2c39ddff203b29f7a4360846d35b76b7880
ismailJihad29 <ismailhossenjihad234@gmail.com>
2023-07-21 11:59:54 +0600

    post toy funtion done

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
-const express = require('express');
-const cors = require('cors');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 
 const app = express();
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
-// middleware 
-app.use(express.json())
-app.use(cors())
+// middleware
+app.use(express.json());
+app.use(cors());
 
-
-
-app.get('/', (req, res) => { 
-    res.send('Figlandia Server Is running ')
-})
-
+app.get("/", (req, res) => {
+  res.send("Figlandia Server Is running ");
+});
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kz2rvmj.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -25,33 +22,47 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-      await client.connect();
+    await client.connect();
 
-      const toysCollection = client.db("figlandiaDb").collection("toys");
+    const toysCollection = client.db("figlandiaDb").collection("toys");
+    const reviewsCollection = client.db("figlandiaDb").collection("reviews");
 
-      app.get("/toys", async(req, res) => { 
-          const result =await toysCollection.find().toArray()
-         res.send(result);
-      })
-    
-    app.get("/toys/:id", async (req, res) => { 
+    app.get("/toys", async (req, res) => {
+      const result = await toysCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/toys/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await toysCollection.findOne(query);
       res.send(result);
+    });
+
+    app.get("/reviews", async (req, res) => { 
+      const result = await reviewsCollection.find().toArray();
+      res.send(result);
+
     })
 
-    // Send a ping to confirm a successful connection
+    app.post("/toys", async (req, res) => { 
+      const toys = req.body
+      console.log('new toy', toys);
+      const result = await toysCollection.insertOne(toys);
+      res.send(result)
+    })
+
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
-    // Ensures that the client will close when you finish/error
     // await client.close();
   }
 }
@@ -60,5 +71,5 @@ run().catch(console.dir);
 
 
 app.listen(port, () => {
-    console.log(`Figladia Server is listening on ${port}`)
-})
+  console.log(`Figladia Server is listening on ${port}`);
+});
